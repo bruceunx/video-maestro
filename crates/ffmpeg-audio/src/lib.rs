4e7c913c837a07c8a3560c8a4c98233ab9c19ef6
bruce <cnhxunoo@gmail.com>
2024-12-22 09:31:17 +0800

    feat: add split wav with hound

--- crates/ffmpeg-audio/src/lib.rs
+++ crates/ffmpeg-audio/src/lib.rs
@@ -1,14 +1,142 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use ffmpeg_next::{self as ffmpeg};
+use hound::{WavReader, WavWriter};
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+pub struct WebmSplitter {
+    chunk_duration: i64,
+}
+
+impl WebmSplitter {
+    pub fn new(duration_seconds: i64) -> Self {
+        Self {
+            chunk_duration: duration_seconds,
+        }
+    }
+
+    pub fn split(&self, input_file: &Path, output_dir: &Path) -> Result<(), ffmpeg::Error> {
+        ffmpeg::init()?;
+        if !output_dir.is_dir() {
+            std::fs::create_dir_all(output_dir).map_err(|_| ffmpeg::Error::Other { errno: -1 })?;
+        }
+
+        let mut input_ctx = ffmpeg::format::input(input_file)?;
+        let audio_stream = input_ctx
+            .streams()
+            .best(ffmpeg::media::Type::Audio)
+            .ok_or(ffmpeg::Error::StreamNotFound)?;
+
+        let audio_stream_index = audio_stream.index();
+        let total_duration = input_ctx.duration() as f64 / f64::from(ffmpeg::ffi::AV_TIME_BASE);
+
+        let num_chunks = (total_duration / self.chunk_duration as f64) as i64;
+
+        let mut output_files = Vec::new();
+
+        let input_codec_params = audio_stream.parameters();
+        let time_base = audio_stream.time_base();
+
+        for chunk_index in 0..num_chunks {
+            let encoder = ffmpeg::codec::encoder::find(input_codec_params.id())
+                .ok_or(ffmpeg::Error::EncoderNotFound)?;
+
+            let start_time = chunk_index * self.chunk_duration;
+
+            let output_filename = format!("chunk_{:03}.webm", chunk_index + 1);
+            let output_path = output_dir.join(output_filename);
+            output_files.push(output_path.clone());
+
+            let mut output_ctx = ffmpeg::format::output(&output_path)?;
+
+            let mut output_stream = output_ctx.add_stream(encoder)?;
+            output_stream.set_time_base(time_base);
+
+            output_ctx.write_header()?;
+
+            let start_ts = start_time * ffmpeg::ffi::AV_TIME_BASE as i64;
+
+            let end_time = ((start_time + self.chunk_duration) * ffmpeg::ffi::AV_TIME_BASE as i64)
+                .min(input_ctx.duration());
+
+            input_ctx.seek(start_ts, ..start_ts)?;
+
+            for (stream, packet) in input_ctx.packets() {
+                if stream.index() == audio_stream_index {
+                    let pts = packet.pts().unwrap_or(0);
+                    let current_time = pts * ffmpeg::ffi::AV_TIME_BASE as i64
+                        / stream.time_base().denominator() as i64;
+
+                    if current_time >= end_time {
+                        break;
+                    }
+
+                    let mut new_packet = packet.clone();
+                    new_packet.set_position(-1);
+                    new_packet.set_stream(0);
+                    new_packet.write_interleaved(&mut output_ctx)?;
+                }
+            }
+
+            output_ctx.write_trailer()?;
+        }
+        Ok(())
+    }
+}
+
+pub fn split_wav(
+    input_file: &Path,
+    output_dir: &Path,
+    chunk_duration_sec: u32,
+) -> Result<(), Box<dyn Error>> {
+    let reader = WavReader::open(input_file)?;
+    let spec = reader.spec();
+    let samples: Vec<i16> = reader.into_samples::<i16>().collect::<Result<_, _>>()?;
+
+    let sample_rate = spec.sample_rate;
+    let channels = spec.channels as usize;
+    let chunk_size = (chunk_duration_sec * sample_rate * channels as u32) as usize;
+
+    if !output_dir.is_dir() {
+        fs::create_dir_all(output_dir)?;
+    }
+
+    for (i, chunk) in samples.chunks(chunk_size).enumerate() {
+        let output_filename = format!("chunk_{:03}.wav", i + 1);
+        let output_file = output_dir.join(output_filename);
+        let mut writer = WavWriter::create(&output_file, spec)?;
+        for &sample in chunk {
+            writer.write_sample(sample)?;
+        }
+        writer.finalize()?;
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use std::{path::PathBuf, str::FromStr};
+
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn split_audio_works() {
+        let audio_splitter = WebmSplitter::new(300);
+
+        let input_file = PathBuf::from_str("./sample.webm").unwrap();
+        let output_dir = PathBuf::from_str("output_dir").unwrap();
+        let result = audio_splitter.split(&input_file, &output_dir);
+
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn split_wav_works() {
+        let input_file = PathBuf::from_str("./output.wav").unwrap();
+        let output_dir = PathBuf::from_str("output_dir").unwrap();
+        let chunk_duration_sec = 120;
+        let result = split_wav(&input_file, &output_dir, chunk_duration_sec);
+        assert!(result.is_ok());
     }
 }
